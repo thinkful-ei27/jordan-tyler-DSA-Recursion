20712572b2c028520f0e6d237464b745edea991f
Tyler Crabb <tydacrabb@gmail.com>
2019-02-25 11:00:12 -0800

    binary rep

---
+++ binaryRep.js
@@ -0,0 +1,11 @@
+// write a recursive function that prints the binary representation of a given number
+
+'use strict';
+
+const binaryRep = (num) => {
+  if(num === 0){
+    return '';
+  }
+  const binary = Math.floor(num%2);
+  return binaryRep(Math.floor(num/2)) + binary;
+};
