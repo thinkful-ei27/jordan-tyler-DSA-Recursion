ccbced05746c82802c14355d7ecb94ad0d2f298f
Tyler Crabb <tydacrabb@gmail.com>
2019-02-25 09:00:00 -0800

    number 1

--- countingSheep.js
+++ countingSheep.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+
+const countSheep = (num) => {
+  if(num === 0){
+    return;
+  }
+  console.log(num + ' Another sheep jumped over the fence');
+  countSheep(num-1);
+};
+
+countSheep(4);
